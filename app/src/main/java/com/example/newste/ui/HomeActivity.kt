aefbce5789536cc6542041ddecf714d5073ab507
Abdelrahman Elbanna <abdelrhmanelbanna35@gmail.com>
2023-09-18 18:14:09 +0200

    created new file  home activity

---
+++ app/src/main/java/com/example/newste/ui/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.newste.ui
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newste.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
