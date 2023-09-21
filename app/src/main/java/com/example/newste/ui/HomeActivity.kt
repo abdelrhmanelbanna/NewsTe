301a257d8cf6d62095da54ac6d5ab3ef2fe87798
Abdelrahman Elbanna <abdelrhmanelbanna35@gmail.com>
2023-09-21 07:26:19 +0300

    created new file  home activity

--- app/src/main/java/com/example/newste/ui/HomeActivity.kt
+++ app/src/main/java/com/example/newste/ui/HomeActivity.kt
@@ -1,22 +1,66 @@
 package com.example.newste.ui
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import android.view.View
+
+import android.widget.ImageView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.example.newste.databinding.ActivityHomeBinding
+import androidx.fragment.app.Fragment
+import com.example.newste.R
 
 class HomeActivity : AppCompatActivity() {
 
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var buttonMenu: ImageView
+
+    lateinit var category: View
+    lateinit var settings: View
+
+    val categoriesFragment = CategoriesFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        initView()
+
+        // by default
+        pushFragment(categoriesFragment)
 
     }
+
+    fun initView() {
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        buttonMenu = findViewById(R.id.menu_button)
+
+        category = findViewById(R.id.category)
+        settings = findViewById(R.id.settings)
+
+        buttonMenu.setOnClickListener {
+            drawerLayout.open()
+        }
+
+        category.setOnClickListener {
+            pushFragment(categoriesFragment)
+        }
+
+        settings.setOnClickListener {
+            pushFragment(SettingsFragment())
+        }
+
+
+    }
+
+
+    fun pushFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_countainer, fragment)
+            .commit()
+
+    }
+
+
 }
